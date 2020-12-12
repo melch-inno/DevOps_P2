6f78b5c007085d3fe43e4d17ee82c4d983d35db9
melch-inno <iba-melch@hotmail.com>
2020-12-13 02:29:51 +0800

    Changing import statement for jolib due to precrated issue

--- Flusk_APP/app.py
+++ Flusk_APP/app.py
@@ -3,7 +3,7 @@ from flask.logging import create_logger
 import logging
 
 import pandas as pd
-import joblib as jlib
+from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
@@ -55,9 +55,9 @@ def predict():
     """
 
     try:
-        clf = jlib.load("./boston_housing_prediction.joblib")
+        clf = joblib.load("./boston_housing_prediction.joblib")
     except:
-        LOG.info("JSON payload: %s json_payload")
+        #LOG.info("JSON payload: %s json_payload")
         return "Model not loaded"
 
     json_payload = request.json
