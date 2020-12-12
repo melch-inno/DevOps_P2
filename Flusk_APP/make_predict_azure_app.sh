e32d60b371f398375f50440b2ff51196257217f6
melch-inno <iba-melch@hotmail.com>
2020-12-13 00:21:33 +0800

    creating main Webapp app service ML prediction

--- Flusk_APP/make_predict_azure_app.sh
+++ Flusk_APP/make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://Flusk_APP.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
